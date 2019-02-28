3d590b9598aa6758bf266e7026034e715ed9ca38
Catalin Manolescu <cc.manolescu@gmail.com>
2019-02-28 23:59:23 +0200

    add spectrum

--- bash/settings.bash
+++ bash/settings.bash
@@ -4,3 +4,11 @@ SAVEHIST=$HISTSIZE
 shopt -s histappend # append to history file
 
 export EDITOR=vim
+
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
--- shell/bootstrap.sh
+++ shell/bootstrap.sh
@@ -1,12 +1,4 @@
 path_prepend "$HOME/.dotfiles/bin"
 path_append "$HOME/bin"
 
-if ! shopt -oq posix; then
-  if [ -f /usr/share/bash-completion/bash_completion ]; then
-    . /usr/share/bash-completion/bash_completion
-  elif [ -f /etc/bash_completion ]; then
-    . /etc/bash_completion
-  fi
-fi
-
 bind 'set bell-style none'
