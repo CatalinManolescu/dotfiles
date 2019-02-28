249e901b5359289ffe76c982936c06282b535469
Catalin Manolescu <cc.manolescu@gmail.com>
2019-02-28 22:17:17 +0200

    add initial configs

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Dotfiles
+
+Based on [https://github.com/anishathalye/dotfiles](https://github.com/anishathalye/dotfiles) and powered by [https://git.io/dotbot](https://git.io/dotbot) .
+
---
+++ shell/aliases.sh
@@ -0,0 +1,13 @@
+# Use colors in coreutils utilities output
+alias ls='ls --color=auto'
+alias grep='grep --color'
+
+# ls aliases
+alias ll='ls -lah'
+
+# Aliases to protect against overwriting
+alias cp='cp -i'
+alias mv='mv -i'
+
+# Mirror a website
+alias mirrorsite='wget -m -k -K -E -e robots=off'
