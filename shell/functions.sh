e94f03591773ee8f24a158efb0b611f9664f4c9f
Catalin Manolescu <cc.manolescu@gmail.com>
2020-01-22 14:39:57 +0200

    filter docker tags by multiple keys

--- shell/functions.sh
+++ shell/functions.sh
@@ -12,52 +12,61 @@ path_prepend() {
     PATH="$1${PATH:+":$PATH"}"
 }
 
-dtags () {
-  if [ $# -lt 1 ]; then
-    echo " 
+dtags_help() {
+  echo " 
     List tags for a docker image available on Docker hub 
     
-    dtags <image> \"[<filter-key>[|<filter-key>[..]]]\" 
+    dtags <image> [<filter-key>[+<filter-key>[..]]] 
     
     Eg.
     - list all tags for nodejs
       /> dtags node
     
-    - list all tags for nodejs that contain the key 'alpine' or 'slim'
-      /> dtags node \"alpine|slim\""
+    - list all tags for nodejs that contain the key 'alpine' and 'lts'
+      /> dtags node alpine+lts
+  "
+}
+
+dtags_filter() {
+  local filter=""
+
+  OIFS=$IFS
+  IFS='+'
+  
+  for filter_value in $1
+  do
+    if [ -n "$filter" ]; then
+    filter=$filter" and "
+    fi
+    filter=$filter"contains(\"${filter_value}\")"
+  done
+
+  if [ -n "$filter" ]; then
+    filter=" | select(.name | $filter )"
+  fi
+
+  IFS=$OIFS
+  echo "$filter"
+}
+
+dtags() {
+  if [ $# -lt 1 ]; then
+    dtags_help
   else
     local image="$1"
-    local tags=`curl -s https://registry.hub.docker.com/v1/repositories/${image}/tags | jq -r '.[].name'`
+    local filter=`dtags_filter $2`
     
-    if [ -n "$2" ]; then
-      tags=`echo -n "$tags" | grep -E "$2"`
-    fi
-
-    echo "$tags"
+    curl -s https://registry.hub.docker.com/v1/repositories/${image}/tags | jq -r ".[]${filter}.name"
   fi
 }
 
 dptags () {
   if [ $# -lt 1 ]; then
-    echo " 
-    List tags for a docker image available on Docker hub 
-    
-    dtags <image> \"[<filter-key>[|<filter-key>[..]]]\" 
-    
-    Eg.
-    - list all tags for nodejs
-      /> dtags node
-    
-    - list all tags for nodejs that contain the key 'alpine' or 'slim'
-      /> dtags node \"alpine|slim\""
+    dtags_help
   else
     local image="$1"
-    local tags=`curl -s https://registry.hub.docker.com/v2/repositories/${image}/tags | jq -r '["tags","size"],["====","===="],(.results[] | [.name,(.images[].size/1024/1024)]) | @tsv' | awk 'BEGIN{ FS=OFS="\t" }NR>2{ $2=sprintf("%.2f",$2) }1'`
-    
-    if [ -n "$2" ]; then
-      tags=`echo -n "$tags" | grep -E "$2"`
-    fi
+    local filter=`dtags_filter $2`
 
-    echo "$tags"
+    curl -s https://registry.hub.docker.com/v2/repositories/${image}/tags | jq -r "[\"tags\",\"size\"],[\"====\",\"====\"],(.results[]${filter} | [.name,(.images[].size/1024/1024)]) | @tsv" | awk 'BEGIN{ FS=OFS="\t" }NR>2{ $2=sprintf("%.2f",$2) }1'
   fi
 }
